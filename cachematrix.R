d97555240e038c2fbd0bf03b6eeced436cbc415c
ozzo <davebox@gmail.com>
2015-02-20 12:05:25 -0500

    Inverse Matrix - Function
    Create function to cached value in case the function is call again. The
    function will check if there are values in the cached, Otherwise it
    will calculate the inverse matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,30 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+      m<-NULL
+      set<-function(y){
+            x<<-y
+            m<<-NULL
+      }
+      get<-function() x
+      setmatrix<-function(solve) m<<- solve
+      getmatrix<-function() m
+      list(set=set, get=get,
+           setmatrix=setmatrix,
+           getmatrix=getmatrix)
 }
 
-
-## Write a short comment describing this function
-
+## The following function will calculate the inverse matrix. But it will check first if the inverse matrix is already calculated, if that is true; Then it will use the cached value.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      m<-x$getmatrix()
+      if(!is.null(m)){
+            message("getting cached data")
+            return(m)
+      }
+      matrix <- x$get() 
+      m<-solve(matrix, ...)
+      x$setmatrix(m)
+      m
 }
